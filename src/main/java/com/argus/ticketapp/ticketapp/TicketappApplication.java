9d8a407579a9dc2d7ce9061ecaffbc70da2f0046
Ranjeet Kumar <88901121+Ranjeet72@users.noreply.github.com>
2024-08-04 11:36:59 +0530

    Third Commit

---
+++ src/main/java/com/argus/ticketapp/ticketapp/TicketappApplication.java
@@ -0,0 +1,13 @@
+package com.argus.ticketapp.ticketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/argus/ticketapp/ticketapp/TicketappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argus.ticketapp.ticketapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
